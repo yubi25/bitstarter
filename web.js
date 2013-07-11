98b7c57d221197c06693d1b7ab1789e75a038d80
yubi25 <yubi25@yahoo.fr>
2013-07-11 21:40:10 +0100

    merde

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
