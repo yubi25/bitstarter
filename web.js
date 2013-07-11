d6445523e25f254b2fea444be4aa5ad6033e85ef
yubi25 <yubi25@yahoo.fr>
2013-07-11 21:42:35 +0100

    merde

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
 var content = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
